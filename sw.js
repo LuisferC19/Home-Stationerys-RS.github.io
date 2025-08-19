823119add8f6e48ca348d21e1411ce417057fe28
＠┊ғᴇʀɴᴀɴᴅᴏ <113485181+LuisferC19@users.noreply.github.com>
2025-08-19 10:05:53 -0600

    Update sw.js
    Signed-off-by: ＠┊ғᴇʀɴᴀɴᴅᴏ <113485181+LuisferC19@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,26 +1,62 @@
-const CACHE_NAME = 'material-tepetitla-v6';
+const CACHE_NAME = 'material-tepetitla-v8'; // Incrementé la versión para forzar la actualización
 const urlsToCache = [
   './',
   './index.html',
-  './producto.html', // <-- NUEVO
+  './producto.html',
   './styles.css',
   './script.js',
-  './producto.js',  // <-- NUEVO
+  './producto.js',
   './productos.json',
   './manifest.json',
   './sw-register.js',
   './img/HomeStatio.png',
-  './img/Audifonos.jpg',
-  './img/Calculadora.jpg',
-  './img/Cargador-iphone.jpg',
-  './img/Goma.jpg',
-  './img/Lapiz.jpg',
-  './img/Lapizero.jpg',
-  './img/Libretas.jpg',
-  './img/Mochila.jpg',
-  './img/Plato-ceramica.jpg',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './img/FaltaImg.png',
+
+  // --- Imágenes de productos que SÍ existen (Lista completa) ---
+  './img/AudifonosCable2.jpg',
+  './img/AudifonosCable3.jpg',
+  './img/PortaCelulares1.jpg',
+  './img/PortaCelulares2.jpg',
+  './img/PortaCelulares3.jpg',
+  './img/CintaAdesiva1.jpg',
+  './img/CintaAdesiva2.jpg',
+  './img/CintaAdesiva3.jpg',
+  './img/ColoresVividel2.jpg',
+  './img/ColoresVividel3.jpg',
+  './img/Corrector2.jpg',
+  './img/Corrector3.jpg',
+  './img/PapelContac1.jpg',
+  './img/PapelContac2.jpg',
+  './img/GomaFactis1.jpg',
+  './img/GomaFactis2.jpg',
+  './img/GomaFactis3.jpg',
+  './img/HojasColor1.jpg',
+  './img/HojasDFomi1.jpg',
+  './img/HojasDFomi2.jpg',
+  './img/JuegoGeometrico1.jpg',
+  './img/LapizBicolor2.jpg',
+  './img/LapizDuo2.jpg',
+  './img/LapizMaped2.jpg',
+  './img/LapizMaped3.jpg',
+  './img/MarcaTexto1.jpg',
+  './img/MarcaTexto2.jpg',
+  './img/MarcaTexto3.jpg',
+  './img/MiniEngrapadoras1.jpg',
+  './img/Plumonesloca241.jpg',
+  './img/Plumonesloca243.jpg',
+  './img/PlumonesWhiteBoard2.jpg',
+  './img/PlumonesWhiteBoard3.jpg',
+  './img/ResitolDixon1.jpg',
+  './img/ResitolDixon2.jpg',
+  './img/ResitolDixon3.jpg',
+  './img/Sacapuntas1.jpg',
+  './img/Sacapuntas3.jpg',
+  './img/FiguraDLego1.jpg',
+  './img/JuegoUno3.jpg',
+  './img/PistolaSilicon1.jpg',
+  './img/PistolaSilicon2.jpg'
 ];
 
 self.addEventListener('install', event => {
@@ -28,7 +64,6 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
     .then(cache => {
         console.log('Cache abierto y archivos añadidos');
-        // El addAll es atómico: si un archivo falla, ninguno se añade.
         return cache.addAll(urlsToCache).catch(error => {
             console.error('Fallo al cachear durante la instalación:', error);
         });
